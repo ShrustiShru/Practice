b993797ba0cdc416960c19dbb1fb258df6340dbe
ShrustiShru <84789190+ShrustiShru@users.noreply.github.com>
2023-02-01 12:44:04 +0530

    Add files via upload

---
+++ Product/Phone.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Products
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class PhoneRepo
+    {
+        public static List<Product> AllProducts => getAll();
+
+        public static List<Product> getAll()
+        {
+            List<Product> product = new List<Product>();
+            product.Add(new Product
+            {
+                ProductId=1,
+                ProductName ="iPhone 14 Pro Max",
+                ProductPrice =175000,
+                Image = @".\Images\iPhone14.jfif"
+            });
+
+            product.Add(new Product
+            {
+                ProductId = 2,
+                ProductName = "Samsung S22",
+                ProductPrice = 180000,
+                Image = @".\Images\Samsung.jfif"
+            });
+
+            product.Add(new Product
+            {
+                ProductId = 3,
+                ProductName = "Google Pixel",
+                ProductPrice = 155000,
+                Image = @".\Images\googlepixel.jfif"
+            });
+
+            product.Add(new Product
+            {
+                ProductId = 4,
+                ProductName = "Moto",
+                ProductPrice = 95000,
+                Image = @".\Images\moto.jfif"
+            });
+
+            product.Add(new Product
+            {
+                ProductId = 5,
+                ProductName = "Samsung Flip",
+                ProductPrice = 125000,
+                Image = @".\Images\flip.jfif"
+            });
+
+            product.Add(new Product
+            {
+                ProductId = 6,
+                ProductName = "Oneplus",
+                ProductPrice = 100000,
+                Image = @".\Images\oneplus.jfif"
+            });
+
+            product.Add(new Product
+            {
+                ProductId = 7,
+                ProductName = "iPad",
+                ProductPrice = 180000,
+                Image = @".\Images\iPad.jpg"
+            });
+
+            return product;
+        }
+    }
+}
---
+++ Product/Product.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Products.cs
+{
+    public partial class Product : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var data = PhoneRepo.AllProducts;//Passing everything to data
+                foreach (var item in data)
+                lstProducts.Items.Add(new ListItem //adds products to listBox created in front end
+                {
+                    Text=item.ProductName,
+                    Value=item.ProductId.ToString()
+                });
+            }
+        }
+
+        protected void lstProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var id = int.Parse(lstProducts.SelectedValue);//input given by a user via dropdown box
+            var selected = PhoneRepo.AllProducts.FirstOrDefault((p) => p.ProductId == id); //comparing user input with the database
+            txtName.Text = selected.ProductName; //after comparing passing the related id details to the id created in the front end and displaying the name ande price
+            txtPrice.Text = selected.ProductPrice.ToString();
+            Image.ImageUrl = selected.Image;
+        }
+
+    }
+}
