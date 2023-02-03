e079cfc26ae735abe95400ea4ff750de21be3b77
ShrustiShru <84789190+ShrustiShru@users.noreply.github.com>
2023-02-03 16:25:30 +0530

    Add files via upload

---
+++ NewsApp/Data Component/INewsRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Data_Component
+{
+    interface INewsRepo
+    {
+        void AddNews(New add);
+        void UpdateNews(int id);
+        void FindNews(New update);
+        List<New> GetAllNews();
+        
+    }
+}
