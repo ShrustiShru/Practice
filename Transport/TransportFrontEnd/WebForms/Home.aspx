0ed27c77c87f05b9d458f97d68194d5f0ebf9250
ShrustiShru <84789190+ShrustiShru@users.noreply.github.com>
2023-02-09 19:03:53 +0530

    Add files via upload

---
+++ Transport/TransportFrontEnd/WebForms/Home.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="TransportFrontEnd.WebForms.Home" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="../Content/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+   <div class="container">
+    <div class="row">
+        <h1>Welcome to Samsung Transport System</h1>
+    </div>
+    <div class="row">
+        <nav>
+            <a href="">Book Ticket</a>
+            <a href="">Employees</a>
+            <a href="">Get Routes</a>
+        </nav>
+    </div>
+   </div>
+</body>
+</html>
---
+++ Transport/TransportLib/Repo/TransportRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransportLib.DataBase;
+using TransportLib.Interface;
+
+namespace TransportLib.Repo
+{
+    class TransportRepo : ITransport
+    {
+        public List<SamsungEmployee> GetAllEmployees(SamsungEmployee employee)
+        {
+            var context = new TransportEntities();
+            var data = context.SamsungEmployees.ToList();
+            return data;
+        }
+
+        public List<RouteName> GetAllRoutes(RouteName routename)
+        {
+            var context = new TransportEntities();
+            var data = context.RouteNames.ToList();
+            return data;
+        }
+
+        public void AddNewRoute(RouteName routename)
+        {
+            var context = new TransportEntities();
+            var data = context.RouteNames.Add(routename);
+            context.SaveChanges();
+        }
+
+
+        public List<RouteStop> GetAllStops(RouteStop routestop)
+        {
+            var context = new TransportEntities();
+            var data = context.RouteStops.ToList();
+            return data;
+        }
+       
+        public void AddNewStops(RouteStop routestop)
+        {
+            var context = new TransportEntities();
+            var data = context.RouteStops.Add(routestop);
+            context.SaveChanges();
+        }
+
+    }
+}
