9e23519728b0df58b750bac94566c5f4c7998488
ShrustiShru <84789190+ShrustiShru@users.noreply.github.com>
2023-02-10 19:33:22 +0530

    Add files via upload

---
+++ Transport/TransportFrontEnd/WebForms/BookTicket.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TransportLib.Repo;
+
+namespace TransportFrontEnd.WebForms
+{
+    public partial class BookTicket : System.Web.UI.Page
+    {
+        TransportRepo repo = new TransportRepo();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtRoute.DataSource = repo.GetAllRoutes();
+                txtRoute.DataTextField = "RouteName1";
+                txtRoute.DataValueField = "RouteNumber";
+                txtRoute.DataBind();
+            }
+        }
+        
+
+        protected void txtRoute_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selected = txtRoute.SelectedValue;
+            txtPick.DataSource = repo.GetAllStops(int.Parse(selected));
+            txtPick.DataTextField = "PickUp";
+            txtPick.DataBind();
+        }
+
+        protected void txtPick_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var selected = txtRoute.SelectedValue;
+            txtDrop.DataSource = repo.GetAllStops(int.Parse(selected));
+            txtDrop.DataTextField = "DropOut";
+            
+            txtDrop.DataValueField = "TicketAmount";
+            txtDrop.DataBind();
+        }
+
+        protected void txtDrop_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            var selected = txtDrop.SelectedValue;
+            lblBook.Text = repo.getFair(txtPick.DataTextField, txtDrop.DataTextField, int.Parse(txtRoute.SelectedValue)).TicketAmount.ToString();
+            lblBook.DataBind();
+        }
+
+        protected void btnBook_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Transport/TransportFrontEnd/WebForms/DisplayEmployees.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TransportLib.DataBase;
+
+namespace TransportFrontEnd.WebForms
+{
+    public partial class DisplayEmployees : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var list = Application["SamsungEmployee"] as List<SamsungEmployee>;
+                lstEmployee.DataSource = Application["SamsungEmployee"] as List<SamsungEmployee>;
+                lstEmployee.DataBind();
+            }
+        }
+    }
+}
